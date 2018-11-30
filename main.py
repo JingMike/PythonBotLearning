57baecdf42bb0fb632b4f406976b8628c16b7261
JingMike <Jing.Mike@qq.com>
2018-11-30 10:09:05 +0800

    V0.01: import from package.

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# main.py
+
+"Test file."
+__author__ = "Jing Mike"
+
+import math
+import time
+import calendar
+import os
+import mypackage.mytool
+
+def myfun():
+    mypackage.mytool.mytoolfun()
+
+if __name__ == "__main__":
+    myfun()
+
+
+
---
+++ mypackage/mytool.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+def mytoolfun():
+    print("hello my tool fun")
+
+if __name__ == "__main__":
+    mytoolfun()
+
